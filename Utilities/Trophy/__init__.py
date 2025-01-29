c918b7bf818ef73aa8f347c34074599469c9abc9
Seregon <109359355+seregonwar@users.noreply.github.com>
2025-01-29 08:53:02 +0100

    Update __init__.py

--- Utilities/Trophy/__init__.py
+++ Utilities/Trophy/__init__.py
@@ -3,4 +3,3 @@ from .Trophy import TrophyFile
 from .TRPCreator import TRPCreator
 from .TRPReader import TRPReader
 from .ESMFDecrypter import ESMFDecrypter
-# Rimuovi eventuali importazioni non necessarie o circolari
